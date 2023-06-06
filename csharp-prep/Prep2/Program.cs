0e453621f624d06fab1b501d78393d2b9cdbd6e0
mnakalr <mwatilenakale@gmail.com>
2023-06-06 22:40:26 +0200

    C# Prep 2 assignment completed

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -3,7 +3,45 @@ using System;
 class Program
 {
     static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Prep2 World!");
+    {   
+        int marks = 0;
+        string letter = "";
+        Console.WriteLine("Enter your marks: ");
+        marks = Convert.ToInt32(Console.ReadLine());
+        string Message = "You failed. Better luck next time!";
+        
+        if (marks >= 90)
+        {
+            letter = "A";
+            Message = "Congradulation, you passed!";
+        }
+        else if (marks >= 80)
+        {
+            letter = "B";
+            Message = "Congradulation, you passed!";
+        }
+        else if (marks >= 70)
+        {
+            letter = "C";
+            Message = "Congradulation, you passed!";
+        }
+        else if (marks >= 60)
+        {
+            letter = "D";
+        }
+        else if (marks >= 50)
+        {
+            letter = "E";
+        }
+        else if (marks < 50)
+        {
+            letter = "F";
+        }
+        else
+        {
+            Console.WriteLine(Message);
+        }
+        Console.WriteLine($"Your grade is: {letter}!");
+        Console.WriteLine(Message);
     }
 }
