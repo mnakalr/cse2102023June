b84e895de1da692567e575f0ca3392a1d8b30b54
mnakalr <mwatilenakale@gmail.com>
2023-06-07 07:18:40 +0200

    C# Prep 3 assignment completed

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -5,5 +5,24 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep3 World!");
+        Console.WriteLine("What is the magic number? ");
+        int magicNumber = int.Parse(Console.ReadLine());  
+        Console.WriteLine("What is your guess? ");  
+        int guess = int.Parse(Console.ReadLine());
+
+        while (guess != magicNumber)
+        {
+            if (guess > magicNumber)
+            {
+                Console.WriteLine("Too high!");
+            }
+            else
+            {
+                Console.WriteLine("Too low!");
+            }
+            Console.WriteLine("What is your guess? ");  
+            guess = int.Parse(Console.ReadLine());
+        }
+        Console.WriteLine($"You guessed it! The magic number is: {magicNumber}");
     }
 }
