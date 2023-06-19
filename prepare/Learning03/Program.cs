6cfc577b284f86dcf2ef355c5d8e69c3f6ce361f
mnakalr <mwatilenakale@gmail.com>
2023-06-19 19:55:14 +0200

    Learning03 assignment completed.

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -1,9 +1,65 @@
 using System;
 
+class Fraction
+{
+    private int _top;
+    private int _bottom;
+
+    public Fraction()
+    {
+        _top = 1;
+        _bottom = 1;
+    }
+
+    public Fraction(int top, int bottom)
+    {
+        _top = top;
+        _bottom = bottom;
+    }
+
+    public Fraction(int wholeNumber)
+    {
+        _top = wholeNumber;
+        _bottom = 1;
+    }
+
+    public void SetTop(int top)
+    {
+        _top = top;
+    }
+
+    public void SetBottom(int bottom)
+    {
+        _bottom = bottom;
+    }  
+
+    public int GetTop()
+    {
+        return _top;
+    }
+
+    public int GetBottom()
+    {
+        return _bottom;
+    }
+
+    public string GetFractionString()
+    {
+        return $"Fraction: {_top}/{_bottom}";
+    }
+
+    public double GetDecimalValue()
+    {
+        return (double)_top / _bottom;
+    }
+}
+
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Learning03 World!");
+        Fraction fraction = new Fraction(1,3);
+        Console.WriteLine(fraction.GetDecimalValue());
     }
 }
