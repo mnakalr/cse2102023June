b586772c0177e4032de5abb66776e0510babe775
mnakalr <mwatilenakale@gmail.com>
2023-07-17 19:26:01 +0200

    added a comment how I exceeded the requirements

--- prove/Develop05/Program.cs
+++ prove/Develop05/Program.cs
@@ -16,7 +16,9 @@ using System.IO;
 class Program
 {
     static void Main(string[] args)
-    {
+    {   
+        // I exceeded the expections by adding an ActionItem class to establish the amount of actions needed to complete a goal.
+        // which makes it easier to track the progress of a goal.
         GoalManager goalManager = new GoalManager();
         goalManager.Start();
     }
